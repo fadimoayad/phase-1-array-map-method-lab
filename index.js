be55db6115763b132900909b1a463dc557ed120c
fadimoayad <fadimoayad99@yahoo.com>
2022-12-05 20:25:13 +0300

    assignment completed

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+return tutorials.map(function(f){
+  let arr=f.split(" ")
+  for(let i = 0; i < arr.length; i++){
+    arr[i]=arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
+  }
+  return arr.join(" ")
+
+}
+)
 }
